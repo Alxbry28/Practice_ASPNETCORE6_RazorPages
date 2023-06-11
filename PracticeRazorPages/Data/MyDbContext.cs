52db36a451747ca92f3cc48048090ef095540ecb
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-06-11 21:16:03 +0800

    [UPDATE] Finish tutorialsEU C# Razor Pages

---
+++ PracticeRazorPages/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeRazorPages.Models;
+
+namespace PracticeRazorPages.Data
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Person> People { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
--- PracticeRazorPages/Program.cs
+++ PracticeRazorPages/Program.cs
@@ -1,7 +1,11 @@
+using PracticeRazorPages.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<MyDbContext>();
 
 var app = builder.Build();
 
